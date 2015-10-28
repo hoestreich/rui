4ff2824112dff970531e23791f5272ac615ed218
Hendrik Oestreich <hoestreich@techfak.uni-bielefeld.de>
2015-10-28 09:31:43 +0100

    Testing Touch

--- src/main/java/de/citec/csra/rui/Main.java
+++ src/main/java/de/citec/csra/rui/Main.java
@@ -20,11 +20,16 @@
  */
 package de.citec.csra.rui;
 
+import java.util.logging.Logger;
+
+import rsb.Informer;
+
 import com.guigarage.responsive.ResponsiveHandler;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
@@ -33,6 +38,7 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -44,67 +50,52 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private static final Logger LOG = Logger
+            .getLogger(Informer.class.getName());
+
     private final int screenwidth = 400;
     private final int screenheight = 400;
+
     @Override
     public void start(final Stage primaryStage) {
         try {
             StackPane root = new StackPane();
             Scene scene = new Scene(root, screenwidth, screenheight);
-            scene.getStylesheets()
-                 .add(getClass()
-                 .getResource("application.css")
-                 .toExternalForm());
+            scene.getStylesheets().add(
+                    getClass().getResource("application.css").toExternalForm());
 
-            Image image = new Image(Main.class.getResourceAsStream("LSP-CSRA_Map_2D_basic.png"));
+            Image image = new Image(
+                    Main.class.getResourceAsStream("LSP-CSRA_Map_2D_basic.png"));
             ImageView imageView = new ImageView();
-            imageView.setFitHeight(screenheight-80);
-            imageView.setFitWidth(screenwidth-80);
-            //imageView.setViewport(new Rectangle2D(40, 40, 100, 100));
+            imageView.setFitHeight(screenheight - 80);
+            imageView.setFitWidth(screenwidth - 80);
+            // imageView.setViewport(new Rectangle2D(40, 40, 100, 100));
             imageView.setImage(image);
-            
-            Slider slider = new Slider();
-            slider.setMin(0);
-            slider.setMax(100);
-            slider.setValue(40);
-            slider.setShowTickLabels(true);
-            slider.setShowTickMarks(true);
-            slider.setMajorTickUnit(50);
-            slider.setMinorTickCount(5);
-            slider.setBlockIncrement(10);
-            slider.valueProperty().addListener(new ChangeListener<Number>() {
-                public void changed(ObservableValue<? extends Number> ov,
-                        Number old_val, Number new_val) {
-                            //imageView.setViewport(new Rectangle2D(new_val.intValue(), imageView.getViewport().getMinY(), 100, 100));
-                    }
-                });
-            Slider slider1 = new Slider();
-            slider1.orientationProperty().set(Orientation.VERTICAL);
-            slider1.setMin(0);
-            slider1.setMax(100);
-            slider1.setValue(40);
-            slider1.setShowTickLabels(true);
-            slider1.setShowTickMarks(true);
-            slider1.setMajorTickUnit(50);
-            slider1.setMinorTickCount(5);
-            slider1.setBlockIncrement(10);
-            slider1.valueProperty().addListener(new ChangeListener<Number>() {
-                public void changed(ObservableValue<? extends Number> ov,
-                        Number old_val, Number new_val) {
-                            //imageView.setViewport(new Rectangle2D(imageView.getViewport().getMinX(), new_val.intValue(), 100, 100));
-                    }
-                });
+            imageView.setOnZoom(new EventHandler<ZoomEvent>() {
+                @Override
+                public void handle(ZoomEvent event) {
+                    imageView.setScaleX(imageView.getScaleX()
+                            * event.getZoomFactor());
+                    imageView.setScaleY(imageView.getScaleY()
+                            * event.getZoomFactor());
+                    LOG.info("Rectangle: Zoom event" + ", inertia: "
+                            + event.isInertia() + ", direct: "
+                            + event.isDirect());
+
+                    event.consume();
+                }
+            });
             HBox hbox = new HBox();
-            hbox.getChildren().addAll(imageView,slider1);
+            hbox.getChildren().addAll(imageView);
             VBox vbox = new VBox();
-            vbox.getChildren().addAll(hbox, slider);
+            vbox.getChildren().addAll(hbox);
             vbox.getStyleClass().addAll("visible-lg", "visible-md");
-            
+
             Label label = new Label();
             label.setText("Fenster zu klein");
             label.getStyleClass().addAll("visible-xs", "visible-sm");
-            
-            root.getChildren().addAll(vbox,label);
+
+            root.getChildren().addAll(vbox, label);
             primaryStage.setScene(scene);
             primaryStage.show();
 
@@ -115,7 +106,8 @@ public class Main extends Application {
     }
 
     /**
-     * @param args argumentlist
+     * @param args
+     *            argumentlist
      */
     public static void main(final String[] args) {
         launch(args);
